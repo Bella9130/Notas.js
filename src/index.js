97e574729101d668f32cf63e98353cda3f8c5d93
Bella <salguero.cano.bellla@gmail.com>
2023-02-26 14:15:05 -0600

    Modificaciones De practica

--- src/index.js
+++ src/index.js
@@ -70,17 +70,17 @@ const productList = [];
 productList.push({
     name:"book-familia",
     price:"200",
-    imagen:"./imagenes/Mi familia y Yo (1).png"
+    imagen:"https://sbuhn.org/wp-content/uploads/2020/07/33-2.jpg"
 });
 productList.push({
     name:"book-nomar",
     price:"200",
-    imagen:'https://scontent.fgua3-3.fna.fbcdn.net/v/t39.30808-6/329157751_3297921150524200_5942539807855017470_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=Xr9yzhZGdREAX-eB1M8&_nc_ht=scontent.fgua3-3.fna&oh=00_AfC8qSit5k1goiYv65IYZoHPTatA0roUht0BaC0GGM-U9Q&oe=63F78289'
+    imagen: 'https://d3j0t7vrtr92dk.cloudfront.net/hccp/1423061224_1403215762_holy_bible_text_11021.png'
 });
 productList.push({
     name:"book nose",
     price:"200",
-    imagen:"https://scontent.fgua3-3.fna.fbcdn.net/v/t39.30808-6/325334296_664836478770159_6939989713696567138_n.jpg?stp=dst-jpg_s1080x2048&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=wQ6wN89sfeEAX_dy97_&_nc_ht=scontent.fgua3-3.fna&oh=00_AfBMTgX0zgF3x2xgG_KcnBci1SNQzmIxlA7phwUKKQecqA&oe=63F71882"
+    imagen:'https://tubiblia.com.co/wp-content/uploads/2019/03/41uPJHV860L-300x466.jpg'
 });
 
 function diferentProducts(arr) {
@@ -126,3 +126,48 @@ function diferentProducts(arr) {
 
 diferentProducts(productList);
 
+
+/* <div class="container-back">
+<div class="div-container-close">
+    <img src="https://static.vecteezy.com/system/resources/thumbnails/009/344/496/small/x-transparent-free-png.png" alt="" class="imagen-aside-1">
+    <img src="imagenes/Mi familia y Yo (1).png" alt="">
+</div>
+<div class="main-container-aside">
+    <p>20$</p>
+    <p>Book</p>
+    <p>producto: Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt neque quia fugiat, quidem magni autem. Ea ipsum mollitia vel, possimus pariatur numquam. Cumque aliquam repellendus tenetur tempore excepturi porro molestias!</p>
+
+    <button class="boton-aside-1 boton-aside-1-s2">
+        <img src="imagenes/creative-light-bulb-ideas-icon.jpg" alt="" class="imagen-aside-2">
+        <a href="/E-MAIL.html" class="a-1">add to cart</a>
+    </button>
+</div>
+</div> */
+
+const differetnProductAside = [];
+differetnProductAside.push ({
+    nameDetailProduct:'Book-familia', 
+    price:'200',
+    imagen:'https://sbuhn.org/wp-content/uploads/2020/07/33-2.jpg',
+    description:'producto: Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt neque quia fugiat, quidem magni autem. Ea ipsum mollitia vel, possimus pariatur numquam. Cumque aliquam repellendus tenetur tempore excepturi porro molestias!' 
+
+});
+differetnProductAside.push ({
+    nameDetailProduct:'Book-familia', 
+    price:'200',
+    imagen:'https://d3j0t7vrtr92dk.cloudfront.net/hccp/1423061224_1403215762_holy_bible_text_11021.png',
+    description: 'producto: Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt neque quia fugiat, quidem magni autem. Ea ipsum mollitia vel, possimus pariatur numquam. Cumque aliquam repellendus tenetur tempore excepturi porro molestias!'
+});
+differetnProductAside.push({
+    nameDetailProduct:'book nose',
+    price:'200',
+    imagen: 'https://tubiblia.com.co/wp-content/uploads/2019/03/41uPJHV860L-300x466.jpg',
+    description: 'producto: Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt neque quia fugiat, quidem magni autem. Ea ipsum mollitia vel, possimus pariatur numquam. Cumque aliquam repellendus tenetur tempore excepturi porro molestias!'
+}) 
+
+function detailProductsAsideShow (arr) {
+    for(Aside of Asides) {
+        
+    }
+}
+ 
