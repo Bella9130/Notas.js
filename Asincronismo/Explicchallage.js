b161b612469ed4a84d5931ff4264bcc134f46802
Bella <salguero.cano.bellla@gmail.com>
2023-04-06 15:15:00 -0600

    Actualizacion de commmit estudio de node

--- Asincronismo/Explicchallage.js
+++ Asincronismo/Explicchallage.js
@@ -8,7 +8,8 @@ el como llamar a una cosa externa por medio del nombre que le pusimos
 dpcument(https://nodejs.org/api/modules.html#requireid) */
 
 //llamado al archivo con el que se llama a API (llamado con require())
-const ExplicXMLHTTPRequest = require('xmlhttprequest')
+const { log } = require('console');
+const ExplicXMLHTTPRequest = require('xmlhttprequest').XMLHttpRequest; 
 
 /* Llamado a API */
 /*para llamar poner link de la API */
@@ -45,17 +46,17 @@ estado que tiene la solicitud y con esto saber cuadno esta disponible la informa
                 callback(null, JSON.parse(ejxhttp.responseText))//aqui dice el valor que de transfomalo a .JSON
 /*responseText regresa el valor dado por el servidor esto es de las propiedades XMLhttpresponse*/
 /*status estado osea que el estado es 200 -parse: transforma la info por quejemplo en este caso a JSON  */
+            }else {//para el manejo de info cuando haya un error
+                const error = new Error('error' + urlApi)//se pone un error y se le pasa 
+    //un error segun sea el caso como la url para decir que el error fue del lado de la API
+                return callback(error, null);
             }
-        }else {//para el manejo de info cuando haya un error
-            const error = new Error('error' + urlApi)//se pone un error y se le pasa 
-//un error segun sea el caso como la url para decir que el error fue del lado de la API
-            return callback(error, null);
         }
         ejxhttp.send()
 
     }
 /*readystate es por decir haci cuando ya no esta viendo la soli sino que ya esta listo  */
-/*Esta Para validar soluciones y errores que puede y va a haber */
+/*Esta Para validar soluciones y errores que puede y va a haber, va a regresar muchos valores en el cambio en el inicio hasta pasar en el que estamos buscando  */
 }
 
 /*Explicacion del 4 detro de los estados hay varios elementos que son 
@@ -74,6 +75,17 @@ ExplicfetchData(`${ExAPI}/products`, function (error1, data1){
     //Si detecta un error que de esto para detener logica
 
 //va a volver a llamar a fetchdata con otro objetivo de poner otra logica consecuente de obener info osea que si la info es correcta y tengo datos ahora dispongo de la info de data1 por lo cual yo puedo pasarla a frchtdata y volvinedola a llamar y pasar otro callback con el valor de error asi como los datos de error o consecuentes(consecuencias)
-    ExplicfetchData(`${ExAPI}/products/${data1[0].id}`)//data1 es el objeto que esta entregando
+    ExplicfetchData(`${ExAPI}/products/${data1[0].id}`, function(error2, data2) /*Para validar error2 */ {
+        //data1 es el objeto que esta entregando
     /*Se pone la API de products y data de datos especificos pero lo que quiero que en el primer elelmento y que pase su id con esto se hace una 2peticion pero al primer elemto del array que trajo la primer peticion   */
-})
+        if(error2) return console.error(error2);
+        /*Se anida otro llamado de fetchdata esperando a obtener info de data2 y poderla utilizar */
+            ExplicfetchData(`${ExAPI}/categories/${data2?.category?.id}`, function(error3, data3){ //?OptionalChaining si lo encuentra algo parecido bien especifi en ES6
+                 //vamos a entrar a categories y luego que en data busque categories . i
+                 if(error3) return console.error(error3);
+                 console.log(data1[0]);
+                 console.log(data2.title);
+                 console.log(data3.name);
+            })
+    });
+});
